c80afbfb572126068f78f0f49fc1a270b6856b0d
sr-ssh <r.sadatshokouhi@gmail.com>
2024-05-07 10:59:09 +0330

    fix: :fire: set state in useLevel for levels

--- src/components/setting/blue/Quality.tsx
+++ src/components/setting/blue/Quality.tsx
@@ -6,7 +6,6 @@ const Quality = () => {
   const { levels, changeLevel, currentLevel } = useLevel();
 
   const $currentLevel = useSignal(currentLevel);
-  const $levels = useSignal(levels);
 
   const setQuality = (index: number) => {
     changeLevel(index);
@@ -17,7 +16,7 @@ const Quality = () => {
       currentItem={$currentLevel}
       setItem={setQuality}
       title="کیفیت پخش"
-      items={$levels?.map((item) => item.level).concat(-1)}
+      items={levels?.map((item) => item.level).concat(-1)}
       iconType="setting"
       lastItemLocale="setting_menu_quality_list_item_auto"
     />
